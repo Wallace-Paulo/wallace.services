160f81d1931716bfaec5b437fdafe6986a93f6c2
Wallace-Paulo <wpdesigeer@gmail.com>
2024-01-17 19:56:55 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+import img from './img/img-perfil.png'
+
+
+function App() {
+
+  return (
+    <>
+      <div className='conteudo'>
+      <div>
+        <img className='img' src={img} alt="Foto do Perfil." />
+        <div className='info-text'>
+          <h1>WALLACE PAULO</h1>
+          <p>DESINER GRÁFICO | FRONT-END | UI/UX DESIGNER</p>
+        </div>
+      </div>
+      <div className='card'>
+        <a className='btn' rel='noreferrer' target="_blank" href="https://wa.me/5521974234181">ORÇAMENTO</a>
+        <a className='btn' rel='noreferrer' target="_blank" href="https://www.instagram.com/wpdesigneer/">INSTAGRAM</a>
+        <a className='btn' rel='noreferrer' target="_blank" href="https://www.linkedin.com/in/wallacepaulo/">LINKEDIN</a>
+        <a className='btn' rel='noreferrer' target="_blank" href="https://www.behance.net/wallacepaulo">PORTIFÓLIO</a>
+      </div>
+      <div className='rodape'>
+        <p>© Wallace Paulo - Todos os direitos reservados.</p>
+      </div>
+      </div>
+      <div className='blur'></div>
+      <div className='blur2'></div>
+    </>
+  )
+}
+
+export default App
